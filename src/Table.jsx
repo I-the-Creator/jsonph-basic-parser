bf3b4638d27806908558bc7288092096f7cec6c5
I-the-Creator <andrey.burluckiy@gmail.com>
2022-01-02 13:07:55 +0100

    table component add

---
+++ src/Table.jsx
@@ -0,0 +1,15 @@
+const Table = () => {
+    return (
+        <div className="table-container">
+            <table>
+                <tbody>
+                    {items.map(item => (
+                        
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
